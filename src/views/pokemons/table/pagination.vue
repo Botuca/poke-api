d531213cca9aee8ba49d165776029cc2aa839fad
botuca <botucacontato@gmail.com>
2022-06-03 18:53:05 -0300

    adicionado icone de pagination e script do fontawesome

--- src/views/pokemons/table/pagination.vue
+++ src/views/pokemons/table/pagination.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <div class="button button__direction">&lt;</div>
+    <div class="button">
+      <i class="fas fa-chevron-left" />
+    </div>
     <div class="button__page">0 ... 15</div>
-    <div class="button button__direction">&gt;</div>
+    <div class="button">
+      <i class="fas fa-chevron-right" />
+    </div>
   </div>
 </template>
 
@@ -23,8 +27,24 @@ export default {
   border-radius: 50%;
   font-weight: bold;
 
+  &:hover {
+    background-color: #333333;
+    color: white;
+    cursor: pointer;
+  }
+
   &__page {
+    display: flex;
+    align-items: center;
     padding: 0px 8px;
+  }
+
+  .fa-chevron-left {
+    padding-right: 3px;
+  }
+
+  .fa-chevron-right {
+    padding-left: 3px;
   }
 }
 </style>

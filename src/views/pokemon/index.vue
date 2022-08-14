f86ba42aa118bc385c02d3f156c03f410367aebb
Botuca <botucacontato@gmail.com>
2022-08-14 18:53:30 -0300

    add pokemon infos

--- src/views/pokemon/index.vue
+++ src/views/pokemon/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="content" :class="pokemonTypePrimaryColor">
-    <span class="content__title">{{ pokemon.name }} </span>
-    <span  class="content__subtitle"> #{{pokemonId}} </span>
-    <img class="content__image" :src="pokemonImageURL" :alt="pokemon.name">
-  </div>
+  <section class="content" :class="typePrimaryColor">
+    <div class="content__intro">
+      <span class="content__intro__title">{{ pokemon.name }}</span>
+      <span class="content__intro__subtitle"> #{{id}}</span>
+      <img class="content__intro__image" :src="imageURL" :alt="pokemon.name">
+    </div>
+    <div class="content__infos">
+      <span> <strong>Abilities:</strong> {{ abilities }} </span>
+      <span> <strong>Types:</strong> {{ types }} </span>
+      <span> <strong>Moves qtd.:</strong> {{ movesLength }} </span>
+      <span> <strong>Height:</strong> {{ height }} m</span>
+      <span> <strong>Weight:</strong> {{ weight }} Kg</span>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -19,21 +28,38 @@ export default {
     }
   },
   computed: {
-    pokemonTypePrimaryColor() {
+    typePrimaryColor() {
       return pokemonsColors[this.fisrtTypeName]?.card;
     },
     fisrtTypeName() {
       return this.pokemon.types?.[0].type.name;
     },
-    pokemonImageURL() {
+    imageURL() {
       return this.pokemon.sprites?.other['official-artwork'].front_default;
     },
-    pokemonId() {
+    id() {
       return `${this.pokemon.id}`.padStart(3, '0');
+    },
+    abilities() {
+      return this.pokemon?.abilities?.map(({ability}) => ability.name).join(', ');
+    },
+    weight() {
+      return this.pokemon.weight / 10;
+    },
+    height() {
+      return this.pokemon.height / 10;
+    },
+    types() {
+      return this.pokemon?.types?.map(({type}) => type.name).join(', ');
+    },
+    movesLength() {
+      return this.pokemon?.moves?.length;
     },
   },
   async mounted() {
     this.pokemon = await getPokemonSearch(`https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}`) || [];
+
+    console.log(this.pokemon);
   },
 }
 </script>
@@ -42,26 +68,36 @@ export default {
 .content {
   height: 100%;
   width: 944px;
-  display: flex;
-  flex-direction: column;
   border: 8px solid #333333;
   border-top: none;
+  display: flex;
+  align-items: center;
 
-  &__title {
-    font-size: 40px;
-    font-weight: bold;
-    padding-left: 16px;
+  &__intro {
+  display: flex;
+  flex-direction: column;
+
+    &__title {
+      font-size: 40px;
+      font-weight: bold;
+      padding-left: 16px;
+    }
+
+    &__subtitle {
+      font-size: 16px;
+      font-weight: bold;
+      padding-left: 16px;
+    }
+
+    &__image {
+      height: 300px;
+      width: 300px;
+    }
   }
 
-  &__subtitle {
-    font-size: 16px;
-    font-weight: bold;
-     padding-left: 16px;
-  }
-
-  &__image {
-    height: 300px;
-    width: 300px;
+  &__infos {
+    display: flex;
+    flex-direction: column;
   }
 }
 </style>
